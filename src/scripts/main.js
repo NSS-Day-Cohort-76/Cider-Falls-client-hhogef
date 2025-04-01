7abd5fb553f73ec089838c9a1d4574d6c3d17bba
Brit Beaty <britneybeaty@gmail.com>
2025-04-01 15:33:30 -0500

    update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,22 +1,17 @@
 import{services} from "./services.js"
 import{guests} from "./guests.js"
 import{areas} from "./areas.js"
-
-const mainContainer = document.querySelector("#container")
-
+const mainContainer = document.getElementById("container")
 
 const serviceHTML = services()
 const guestsHTML = guests()
 const areasHTML = areas()
-
-export const renderHTML = () => {
+const renderHTML = () => {
   return `
-    <h1 id="services_header">Services provided</h1>
+    <h1 id="services_header">Cider Falls</h1>
     <div id="services-list">${serviceHTML}</div>
-
     <div id="areas_related_amenities">
-    <h1>${areasHTML}</h1> 
-    <h2>Amenities</h2>
+    <p>${areasHTML}</p> 
     </div>
     <div id="guest">
     <h1>Current Park Guest</h1>
@@ -24,4 +19,35 @@ export const renderHTML = () => {
     </div>
   `
 }
-mainContainer.innerHTML = renderHTML()
+ mainContainer.innerHTML = renderHTML()
+
+
+
+// import{services} from "./services.js"
+// import{guests} from "./guests.js"
+// import{areas} from "./areas.js"
+
+// const mainContainer = document.querySelector("#container")
+
+
+// const serviceHTML = services()
+// const guestsHTML = guests()
+// const areasHTML = areas()
+
+// export const renderHTML = () => {
+//   return `
+//     <h1 id="services_header">Services provided</h1>
+//     <div id="services-list">${serviceHTML}</div>
+
+//     <div id="areas_related_amenities">
+//     <h1>${areasHTML}</h1> 
+//     <h2>Amenities</h2>
+//     </div>
+//     <div id="guest">
+//     <h1>Current Park Guest</h1>
+//     <p>${guestsHTML}</p>
+//     </div>
+//   `
+// }
+
+// mainContainer.innerHTML = renderHTML()
