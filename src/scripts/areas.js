b142f7d9c4a2ebeee41cc3f22dfe9a2c53acb4e4
Brit Beaty <britneybeaty@gmail.com>
2025-04-02 14:08:51 -0500

    added a lot of CSS and fixed clickEvent

--- src/scripts/areas.js
+++ src/scripts/areas.js
@@ -8,7 +8,7 @@ export const areas = () => {
  let areasHTML = `<div class="areas-container">`
 
     for (const area of areasArray){
-      areasHTML+= `<div class="area-card" data-type="areas" data-name="${area.titleName}" data-id="$area.id}">`
+      areasHTML+= `<div class="area-card" data-type="areas" data-name="${area.titleName}" data-id="${area.id}">`
       areasHTML += `<h1>${area.titleName}</h1>`
       serve.forEach(serve => { 
         if (serve.areasIn.includes(area.id)){
